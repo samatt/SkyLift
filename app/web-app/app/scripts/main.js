14a709e14cb9d995f6d58d105fd71dabade94fae
Surya Mattu <surya@suryamattu.com>
2014-11-05 17:34:13 -0500

    hover added

--- app/web-app/app/scripts/main.js
+++ app/web-app/app/scripts/main.js
@@ -15,14 +15,14 @@
 
 $(document).ready(function() {
 	
-	var viewer, scene, primitives;
+	var viewer, scene, primitives, handler, boards;
 	
 	
 	var skylift = {
 		
 		entries: [],
 		loadID: 0,
-				
+
 		// --------------------------------------------------------------------------------
 		
 		loadFileList: function( ){
@@ -37,8 +37,8 @@ $(document).ready(function() {
 					console.log('removing');
 					$('li.network-name').remove();
 					$('#network-list-title').html('Loading...');
-			    	skylift.loadSSIDList($(this).find( 'option:selected' ).eq(0).val());
-			  });
+					skylift.loadSSIDList($(this).find( 'option:selected' ).eq(0).val());
+				});
 			});
 		},
 
@@ -47,9 +47,9 @@ $(document).ready(function() {
 		
 		loadSSIDList: function( file ){
 			$.getJSON( '_private/data/logs/' + file, function( data ) {
-					skylift.entries =[];
+				skylift.entries =[];
 				$.each(data, function(key,entry){
-				
+
 					skylift.entries.push( entry );
 				});
 				skylift.onLoadSSIDList();
@@ -71,10 +71,10 @@ $(document).ready(function() {
         "name": "Justin's iPad"
     }, 
     */
-		
-		onLoadSSIDList: function(){
-			$('#network-list-title').html('Networks');
-			console.log('onLoadSSIDList');
+
+    onLoadSSIDList: function(){
+    	$('#network-list-title').html('Networks');
+    	console.log('onLoadSSIDList');
 			//var count = 0;
 
 			var ssidEL = $('ul.networks');
@@ -97,29 +97,29 @@ $(document).ready(function() {
 				node += '	<span class="l2">'; // hide elements until clicked
 				
 				if( !(
-				entry.geo === null || entry.geo === undefined
-				|| entry.geo.lat === undefined
-				|| entry.geo.lon === undefined
-				|| entry.geo.lat === null
-				|| entry.geo.lon === null
-				|| entry.geo.lon === ''
-				|| entry.geo.lat === ''
-				) ){
+					entry.geo === null || entry.geo === undefined
+					|| entry.geo.lat === undefined
+					|| entry.geo.lon === undefined
+					|| entry.geo.lat === null
+					|| entry.geo.lon === null
+					|| entry.geo.lon === ''
+					|| entry.geo.lat === ''
+					) ){
 					node += '		<ul class="network-info">';
-					node += '			<li>Network Info</li>';
-					node += '		</ul>';
+				node += '			<li>Network Info</li>';
+				node += '		</ul>';
 				
-					node += '		<ul class="geo">';
-					node += '			<li class="latlong" data-net-uid="' + entry.uid +'"><a href="#">' + entry.geo.lat + ', ' + entry.geo.lon + '</a></li>';
-					node += '		</ul>';
-				}
+				node += '		<ul class="geo">';
+				node += '			<li class="latlong" data-net-uid="' + entry.uid +'"><a href="#">' + entry.geo.lat + ', ' + entry.geo.lon + '</a></li>';
+				node += '		</ul>';
+			}
+
+			if( entry.ids !== null && entry.ids.length > 0 ){
+				node += '		<ul class="network-info">';
+				node += '			<li>Associated MACs</li>';
+				node += '		</ul>';
 				
-				if( entry.ids !== null && entry.ids.length > 0 ){
-					node += '		<ul class="network-info">';
-					node += '			<li>Associated MACs</li>';
-					node += '		</ul>';
-				
-					node += '		<ul class="mac">';
+				node += '		<ul class="mac">';
 					// for each network mac id
 					$.each( entry.ids, function(k2,id){
 						node += '			<li><a href="#">' + id + '</a>';
@@ -142,14 +142,14 @@ $(document).ready(function() {
 				ssidEL.append(node);
 				node = null;
 			});
-			
-			console.log('set mouse events');
-			this.attachSSIDMouseEvents();
-			
-			console.log('set network markers');
+
+console.log('set mouse events');
+this.attachSSIDMouseEvents();
+
+console.log('set network markers');
 			setGeoMarkers(); // function hanging outside scope, fix later
 		},
-						
+
 		// --------------------------------------------------------------------------------
 		attachSSIDMouseEvents: function(){
 			// mouse events
@@ -157,15 +157,15 @@ $(document).ready(function() {
 			$('ul.networks > li.network-name > span > a').each( function(){
 				//console.log('appending mouse: ' + count++);
 				$(this).click(function() {					
-				  $(this).toggleClass('active').parent().parent().find('.l2').slideToggle( 'fast', function() {
-				  });
+					$(this).toggleClass('active').parent().parent().find('.l2').slideToggle( 'fast', function() {
+					});
 				});
 			});
 			
 			$('ul.mac > li a').each( function(){
 				$(this).click(function() {
 					// toggle css class
-				  $(this).parent().find('ul.ssid-mac').slideToggle( 'fast', function() {
+					$(this).parent().find('ul.ssid-mac').slideToggle( 'fast', function() {
 				  	console.log($(this).parent().find('a').html()); // outputs MAC addr
 				  }).parent().find('a').eq(0).toggleClass('active');
 				});
@@ -175,33 +175,33 @@ $(document).ready(function() {
 			$('ul.ssid-mac > li a').each( function(){
 				$(this).click(function() {
 					// toggle css class
-				  var netUID = $(this).parent().attr('data-net-uid');
-				  var parentUID = $(this).parent().attr('data-parent-uid');
-				  var pn = $('li.network-name[data-net-uid="' + parentUID + '"] .l2');
-				  var nn = $('li.network-name[data-net-uid="' + netUID + '"] .l2');
-				  pn.find('.mac a').removeClass('active');
-				  pn.find('.ssid-mac').hide();
+					var netUID = $(this).parent().attr('data-net-uid');
+					var parentUID = $(this).parent().attr('data-parent-uid');
+					var pn = $('li.network-name[data-net-uid="' + parentUID + '"] .l2');
+					var nn = $('li.network-name[data-net-uid="' + netUID + '"] .l2');
+					pn.find('.mac a').removeClass('active');
+					pn.find('.ssid-mac').hide();
 				  //console.log('click: ' + $(this).html() + ', netUID: ' + netUID + ', parentUID: ' + parentUID);
-					$('li.network-name[data-net-uid="' + parentUID + '"] > a').toggleClass('active');
-						
-					  pn.slideToggle( 'fast', function() {
-					  	$(this).find('li.latlong a').removeClass('active');
-					  	
-					  	nn.slideToggle( 'fast', function() {
-					  		$('#sidebar').scrollTo( $('li.network-name[data-net-uid="' + netUID + '"]'), 1000, {
-						  		offset:-50,
-						  		easing:'easeOutCubic',
-						  		onAfter:function(){ 
+				  $('li.network-name[data-net-uid="' + parentUID + '"] > a').toggleClass('active');
+
+				  pn.slideToggle( 'fast', function() {
+				  	$(this).find('li.latlong a').removeClass('active');
+
+				  	nn.slideToggle( 'fast', function() {
+				  		$('#sidebar').scrollTo( $('li.network-name[data-net-uid="' + netUID + '"]'), 1000, {
+				  			offset:-50,
+				  			easing:'easeOutCubic',
+				  			onAfter:function(){ 
 							  		// console.log('done');		
-						  		}
-					  		});
-					  		$(this).parent().find('a').eq(0).addClass('active');
-					  	});
-					  	$(this).parent().find('a').eq(0).removeClass('active');
-					  });
+							  	}
+							  });
+				  		$(this).parent().find('a').eq(0).addClass('active');
+				  	});
+				  	$(this).parent().find('a').eq(0).removeClass('active');
+				  });
 				});
-			});
-			
+});
+
 			// Lat/Long clickable
 			$('li.latlong a').each( function(){
 				$(this).click( function(){
@@ -226,21 +226,21 @@ $(document).ready(function() {
 	  // --------------------------------------------------------------------------------
 	  start: function(){
 	  	console.log('SKYLIFT');
-		  this.loadFileList();
+	  	this.loadFileList();
 	  },
 	  
 	  getEntryBySSID: function(ssid){
-		  $.each(skylift.entries, function(key,val){
+	  	$.each(skylift.entries, function(key,val){
 		  		//console.log('does it match? ' + ssid + ' ? ' + val.geo.bssid);
-			  	if( val.geo.bssid === ssid ){
-			  		console.log('match!');
-				  	return val;
-			  	}
-		  });
-		  return null;
+		  		if( val.geo.bssid === ssid ){
+		  			console.log('match!');
+		  			return val;
+		  		}
+		  	});
+	  	return null;
 	  }
-  
-  };
+
+	};
 	
 	
 	
@@ -252,7 +252,7 @@ $(document).ready(function() {
 	window.viewer = viewer;
 	
 	$( window ).resize(function() {
-  	$( '#cesium-parent-container' ).css('height',$(window).height() );
+		$( '#cesium-parent-container' ).css('height',$(window).height() );
 	});
 	$( window ).resize();
 	// Begin
@@ -279,13 +279,13 @@ $(document).ready(function() {
 	
 	function onCesiumReady(){
 		try{
-				viewer = new Cesium.Viewer('cesiumContainer');
-				scene = viewer.scene;
-				primitives = scene.primitives;
-				cesiumInited = true;
-				console.log('ok, init');
-			} catch(e){
-			}
+			viewer = new Cesium.Viewer('cesiumContainer');
+			scene = viewer.scene;
+			primitives = scene.primitives;
+			cesiumInited = true;
+			console.log('ok, init');
+		} catch(e){
+		}
 	}
 	
 	try {
@@ -299,50 +299,90 @@ $(document).ready(function() {
 	}
 	
 	function getRandomInt(min, max) {
-  		return Math.floor(Math.random() * (max - min)) + min;
+		return Math.floor(Math.random() * (max - min)) + min;
 	}
 	// Getting messy
 	function setGeoMarkers(){
-			
-			if( cesiumInited !== true ){
-					isCesiumReady();
-			}
-			
-	    var canvas = document.createElement('canvas');
-	    canvas.width = 10;
-	    canvas.height = 10;
-	    var context2D = canvas.getContext('2d');
-	    context2D.beginPath();
+
+		if( cesiumInited !== true ){
+			isCesiumReady();
+		}
+
+
+		var canvas = document.createElement('canvas');
+		canvas.width = 10;
+		canvas.height = 10;
+		var context2D = canvas.getContext('2d');
+		context2D.beginPath();
 	    context2D.arc(4, 4, 4, 0, Cesium.Math.TWO_PI, true); // ?
 	    context2D.closePath();
 	    context2D.fillStyle = 'rgb(255, 255, 255)';
 	    context2D.fill();
-		
-		//remove old prii
-		primitives.removeAll();
-	    var billboards = primitives.add(new Cesium.BillboardCollection());
-	    var count = 0;
 
-	    $.each( skylift.entries, function(key,entry){
-	    		if( Math.abs(entry.geo.lat) > 0 && Math.abs(entry.geo.lon) > 0 ){
-			    	billboards.add({
-			        imageId : 'Point ' + count + ', uid: ' + entry.uid,
-			        image : canvas,
-			        position : Cesium.Cartesian3.fromDegrees(entry.geo.lon, entry.geo.lat), // long, lat
-			        color : Cesium.Color.RED
-						});
-						count++;
+		//remove old primitives
+		primitives.removeAll();
+		boards = {};
+		
+		var billboards = primitives.add(new Cesium.BillboardCollection());
+		var count = 0;
+
+
+		$.each( skylift.entries, function(key,entry){
+
+			if( Math.abs(entry.geo.lat) > 0 && Math.abs(entry.geo.lon) > 0 ){
+				var b  = billboards.add({
+					imageId : 'Point ' + count + ', uid: ' + entry.uid,
+					id :entry.uid,
+					image : canvas,
+				        position : Cesium.Cartesian3.fromDegrees(entry.geo.lon, entry.geo.lat), // long, lat
+				        color : Cesium.Color.RED
+				    });
+				count++;
+				boards[entry.uid.toString()] = b;
+			}
+		});
+
+	    //remove old event handler
+	    handler = handler && handler.destroy();
+	    handler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
+	    handler.setInputAction(
+	    	function (movement) {
+				
+					$.each(boards, function(key, value) {
+						value.color = Cesium.Color.RED;		 	
+					});
+
+	            	var pickedObject = scene.pick(movement.endPosition);
+	            	if(Cesium.defined(pickedObject)){
+
+	            		if (Cesium.defined(pickedObject) && (pickedObject.primitive === boards[pickedObject.id.toString()])) {
+
+							boards[pickedObject.id.toString()].color =Cesium.Color.WHITE;
+							$.each( skylift.entries, function(key,entry){
+								if(entry.uid == pickedObject.id ){
+									console.log(entry);
+									$('#network-list-title').html("Current : "+ entry.name);
+
+								}
+							});
+						} 
 					}
-	    });
+					else{
+						$('#network-list-title').html('Networks');
+					}
+				},
+				Cesium.ScreenSpaceEventType.MOUSE_MOVE
+				);
+	    
 	    drawAllLines();
-	 }
-	 
+	}
+
 	 // Fly to location
 	 function flyToRectangle( entry ) {
-	 
-	 		if( cesiumInited !== true ){
-					isCesiumReady();
-			}
+
+	 	if( cesiumInited !== true ){
+	 		isCesiumReady();
+	 	}
 	 		// paramter is network object form JSON
 	 		console.log('flyToRectangle: ' + entry.uid + ' , lat: ' + entry.geo.lat + ', lon: ' + entry.geo.lon + ', name: ' + entry.name);
 	 		
@@ -350,39 +390,39 @@ $(document).ready(function() {
 		 	var fuzzShape = 0.015; // degrees 
 		 	var fuzzZoom = fuzzShape + 0.0015 // degrees
 		 	
-		  var lon = parseFloat(entry.geo.lon);
-		  var lat = parseFloat(entry.geo.lat);
-		  
-		  var rectZoom = {
-				west: lon - fuzzZoom,
-				south: lat - fuzzZoom,
-				east: lon + fuzzZoom,
-				north: lat + fuzzZoom
-		  };
-		  
-		  var rectShape = {
-				west: lon - fuzzShape,
-				south: lat - fuzzShape,
-				east: lon + fuzzShape,
-				north: lat + fuzzShape
-		  };
-		  
-		  console.dir(rectZoom);
-		
-		  scene.camera.flyToRectangle({
-		      destination : Cesium.Rectangle.fromDegrees(rectZoom.west, rectZoom.south, rectZoom.east, rectZoom.north)
-		  });
-		
+		 	var lon = parseFloat(entry.geo.lon);
+		 	var lat = parseFloat(entry.geo.lat);
+
+		 	var rectZoom = {
+		 		west: lon - fuzzZoom,
+		 		south: lat - fuzzZoom,
+		 		east: lon + fuzzZoom,
+		 		north: lat + fuzzZoom
+		 	};
+
+		 	var rectShape = {
+		 		west: lon - fuzzShape,
+		 		south: lat - fuzzShape,
+		 		east: lon + fuzzShape,
+		 		north: lat + fuzzShape
+		 	};
+
+		 	console.dir(rectZoom);
+
+		 	scene.camera.flyToRectangle({
+		 		destination : Cesium.Rectangle.fromDegrees(rectZoom.west, rectZoom.south, rectZoom.east, rectZoom.north)
+		 	});
+
 		  // Show the rectangle.  Not required; just for show.
 		  var polylines = scene.primitives.add(new Cesium.PolylineCollection());
 		  polylines.add({
-		      positions : Cesium.Cartesian3.fromDegreesArray([
-		          rectShape.west, rectShape.south,
-		          rectShape.west, rectShape.north,
-		          rectShape.east, rectShape.north,
-		          rectShape.east, rectShape.south,
-		          rectShape.west, rectShape.south
-		      ])
+		  	positions : Cesium.Cartesian3.fromDegreesArray([
+		  		rectShape.west, rectShape.south,
+		  		rectShape.west, rectShape.north,
+		  		rectShape.east, rectShape.north,
+		  		rectShape.east, rectShape.south,
+		  		rectShape.west, rectShape.south
+		  		])
 		  });
 		  
 		}
@@ -391,7 +431,7 @@ $(document).ready(function() {
 	// Example 1: Draw a red polyline on the globe surface
 	function drawAllLines(){
 		$.each( skylift.entries, function(key,entry){
-		
+
 			$.each( entry.ids, function(idKey, id){
 				var dest = skylift.getEntryBySSID(id);
 				
@@ -400,24 +440,24 @@ $(document).ready(function() {
 					
 					// add a line connecting every point
 					scene.primitives.add(new Cesium.Primitive({
-					    geometryInstances : new Cesium.GeometryInstance({
-					        geometry : new Cesium.PolylineGeometry({
-					            positions : Cesium.Cartesian3.fromDegreesArray([
-					                entry.geo.lon, entry.geo.lat,
-					                dest.geo.lon, dest.geo.lat
-					            ]),
-					            width : 2.0,
-					            vertexFormat : Cesium.PolylineColorAppearance.VERTEX_FORMAT
-					        }),
-					        attributes: {
-					            color: Cesium.ColorGeometryInstanceAttribute.fromColor(new Cesium.Color(1.0, 0.0, 0.0, 1.0))
-					        }
-					    }),
-					    appearance : new Cesium.PolylineColorAppearance()
+						geometryInstances : new Cesium.GeometryInstance({
+							geometry : new Cesium.PolylineGeometry({
+								positions : Cesium.Cartesian3.fromDegreesArray([
+									entry.geo.lon, entry.geo.lat,
+									dest.geo.lon, dest.geo.lat
+									]),
+								width : 2.0,
+								vertexFormat : Cesium.PolylineColorAppearance.VERTEX_FORMAT
+							}),
+							attributes: {
+								color: Cesium.ColorGeometryInstanceAttribute.fromColor(new Cesium.Color(1.0, 0.0, 0.0, 1.0))
+							}
+						}),
+						appearance : new Cesium.PolylineColorAppearance()
 					}));
-					}
-				});
+				}
 			});
+		});
 
 	}
 
