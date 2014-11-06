d0a882731f7696e0d11e1963cafacc7afcfceef7
Adam Harvey <adam@ahprojects.com>
2014-11-05 21:05:46 -0500

    Added combined build file

---
+++ app/build.sh
@@ -0,0 +1,12 @@
+# Build the web app, clear the old node-app, build the new node-app, open the node-app
+echo "1/3 Build web-app distribution"
+cd web-app
+grunt build
+echo "2/3 Clear build cache"
+cd ../node-app
+rm -rf build
+echo "3/3 Build node-app distribution"
+grunt nodewebkit
+echo "Done. Opening nodewebkit app."
+open ./node-app/build/skylift/osx/skylift.app
+
